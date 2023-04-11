615a242cbbebf103d3f6a517429ef9ab78682342
Dharmik <61076480+dharmik3148@users.noreply.github.com>
2023-04-11 23:46:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import ReactPlayer from "react-player";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./components/Navbar";
+import MainSection from "./components/MainSection";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        theme="light"
+      />
+      <Navbar />
+      <MainSection />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Content.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <>
+      <div className="cat">
+        <li>
+          <i className="bi bi-camera-reels-fill"></i>
+          <label>Video</label>
+        </li>
+        <li>
+          <i className="bi bi-image"></i>
+          <label>Photo</label>
+        </li>
+        <li>
+          <i className="bi bi-play-circle-fill"></i>
+          <label>Story</label>
+        </li>
+        <li>
+          <i className="bi bi-film"></i>
+          <label>Reel</label>
+        </li>
+        <li>
+          <i className="bi bi-tv"></i>
+          <label>IGTV</label>
+        </li>
+      </div>
+      <p className="sec-txt-1">Instagram Download</p>
+      <p className="sec-txt-2">
+        Download Video Instagram, Photo, Reels, Stories, IGTV online
+      </p>
+    </>
+  );
+};
+
+export default Content;
